e602f854947e4acdb1685402d334a6ac2adcf461
nasaawakening <nasaawakening@gmail.com>
2026-03-07 13:46:05 +0700

    Create MainActivity.kt

---
+++ app/src/main/java/com/Inai/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.inai
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.inai.chat.ChatScreen
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+
+        setContent {
+
+            ChatScreen()
+
+        }
+    }
+
+}
